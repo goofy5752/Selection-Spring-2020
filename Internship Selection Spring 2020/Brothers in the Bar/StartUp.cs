3f141f1ea9b1d807697bb5abe219f5b3139305ca
Martin Asenov <m.asenov5752@gmail.com>
2020-02-12 12:36:15 +0200

    Added explanation for each line of logic

--- Internship Selection Spring 2020/Brothers in the Bar/StartUp.cs
+++ Internship Selection Spring 2020/Brothers in the Bar/StartUp.cs
@@ -25,25 +25,30 @@
         private static int BrothersInTheBarVariant1(List<int> glasses)
         {
             //initialize the counter for consecutive integers
-            var counter = 0;
+            int consecutiveGlasses = 0;
 
             //iterate through array of integers
             for (int i = 2; i < glasses.Count; i++)
             {
-                var firstNum = glasses[i - 2];
-                var secondNum = glasses[i - 1];
-                var currentNum = glasses[i];
+                //initialize the variables that will contains the accordingly number
+                int firstNum = glasses[i - 2];
+                int secondNum = glasses[i - 1];
+                int currentNum = glasses[i];
 
                 //check if the 3 consecutive numbers are equal
-                if (firstNum == secondNum && secondNum == currentNum)
-                {
-                    glasses.RemoveRange(i - 2, 3);
-                    i = 2;
-                    counter++;
-                }
+                if (firstNum != secondNum || secondNum != currentNum) continue;
+
+                //remove the numbers from array
+                glasses.RemoveRange(i - 2, 3);
+
+                //set the iterator at initial value to start from beginning
+                i = 1;
+
+                //increment the counter for consecutive glasses
+                consecutiveGlasses++;
             }
 
-            return counter;
+            return consecutiveGlasses;
         }
 
         private static int BrothersInTheBarVariant2(List<int> glasses)
@@ -51,19 +56,30 @@
             //initialize the counter for consecutive integers
             int consecutiveGlasses = 0;
 
+            //loop while there is consecutive numbers
             while (true)
             {
+                //initialize the boolean variable to check if there is consecutive numbers
                 bool isArrayContainsConsecutiveGlasses = false;
 
+                //iterate through array of integers
                 for (int i = glasses.Count - 1; i >= 2; i--)
                 {
+                    //check if there is 3 consecutive numbers if not continue
                     if (glasses[i] != glasses[i - 1] || glasses[i - 1] != glasses[i - 2]) continue;
+
+                    //removes the numbers from the array
                     glasses.RemoveRange(i - 2, 3);
+
+                    //increment the counter for consecutive glasses
                     consecutiveGlasses++;
+
+                    //set the boolean to true and break the for loop
                     isArrayContainsConsecutiveGlasses = true;
                     break;
                 }
 
+                //if there is no 3 consecutive numbers break the while and return the counter
                 if (isArrayContainsConsecutiveGlasses == false)
                 {
                     break;
